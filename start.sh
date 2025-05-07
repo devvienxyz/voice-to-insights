0eb71cd3a8fbbefa663761a9677802e2687a04ab
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-07 13:29:50 +0800

    Simplify start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python app/warmup_models.py
-streamlit run app/main.py --server.port 8501 --server.address localhost --server.runOnSave=true --logger.level=debug --global.developmentMode=false --server.fileWatcherType=none
+# python app/warmup_models.py
+streamlit run app/main.py
