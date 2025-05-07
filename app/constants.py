e7cbc83552df5df13dcad82ab9db1052e513f145
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-07 13:48:30 +0800

    Moved constants to constants py

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,16 @@
 from enum import Enum
 
+
 class TextSummarizationModels(Enum):
     default = "sshleifer/distilbart-cnn-12-6"
+
+
+RECORDINGS_DIR = "recordings"
+SOUND_WINDOW_LEN = 5000  # 5 seconds
+
+# --- Configuration ---
+AUDIO_RECEIVER_SIZE = 4096
+MEDIA_STREAM_CONSTRAINTS = {"audio": True}
+WEBRTC_KEY = "sendonly-audio"
+PAGE_TITLE = "Audio Summarizer"
+APP_TITLE = "🎤 Voice -> Summary + Action Items"
